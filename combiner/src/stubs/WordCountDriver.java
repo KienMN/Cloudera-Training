81d8c47313f48e5e59ff3e24348f0e64cde2d4f0
training <training@localhost.localdomain>
2017-11-09 08:59:21 -0500

    complete combiner

--- combiner/src/stubs/WordCountDriver.java
+++ combiner/src/stubs/WordCountDriver.java
@@ -1,7 +1,12 @@
 package stubs;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -27,6 +32,15 @@ public class WordCountDriver extends Configured implements Tool {
     /*
      * TODO implement
      */
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setMapperClass(WordMapper.class);
+    job.setReducerClass(SumReducer.class);
+    job.setCombinerClass(SumReducer.class);
+    
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
 
     if (job.getCombinerClass() == null) {
       throw new Exception("Combiner not set");
